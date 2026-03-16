b4473fa466512bfa445bff3fc2989d9221c52e4d
alperen <alprnemn@hotmail.com>
2026-03-16 13:46:39 +0300

    api gateway implemented

---
+++ Makefile
@@ -0,0 +1,4 @@
+run-gway:
+	@go run services/apigateway/cmd/main.go
+run-user:
+	@go run services/user/cmd/main.go
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"os"
+	"strconv"
+)
+
+// GetString gets config as a string using key
+func GetString(key, fallback string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	return value
+}
+
+// GetInt gets config as a int using key
+func GetInt(key string, fallback int) int {
+
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	valInt, err := strconv.Atoi(value)
+	if err != nil {
+		return fallback
+	}
+	return valInt
+}
+
+// GetBool gets bool env value
+func GetBool(key string, fallback bool) bool {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	boolVal, err := strconv.ParseBool(value)
+	if err != nil {
+		return fallback
+	}
+	return boolVal
+}
